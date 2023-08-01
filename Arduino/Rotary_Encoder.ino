a0c44afc9c6339b946a1cd91fd01e70e66d20d17
daniel-dunn <dannyboydunn@gmail.com>
2023-08-01 02:25:04 +0100

    Added code files for arduino and for raspberry pi

---
+++ Arduino/Rotary_Encoder.ino
@@ -0,0 +1,29 @@
+#define outputA 7
+#define outputB 6
+
+int counter = 0;
+int aState;
+int aLastState;
+
+
+void setup() {
+  pinMode(outputA, INPUT);
+  pinMode(outputB, INPUT);
+
+  Serial.begin(9600);
+
+  aLastState = digitalRead(outputA);
+}
+
+void loop() {
+  aState = digitalRead(outputA);
+  if(aState != aLastState){
+    if(digitalRead(outputB) != aState){
+      counter++;
+    } else{
+      counter--;
+    }
+    Serial.print(counter);
+  }
+  aLastState = aState;
+}
