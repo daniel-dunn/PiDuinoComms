53c548bf816a0ea69fc6c1a0febc2eb7384c08e9
daniel-dunn <dannyboydunn@gmail.com>
2023-08-06 13:20:29 +0100

    Revert "broke GUI while trying to fix it"
    This reverts commit 670595929c707ffc98e2ded1372c03edd94b9990.

--- RaspberryPi/SimplifiedDisplayEnoderOutput.py
+++ RaspberryPi/SimplifiedDisplayEnoderOutput.py
@@ -12,16 +12,14 @@ class App(tk.Tk):
         self.geometry('600x300+100+100')
         self.textbox = scrolledtext.ScrolledText(self, wrap=tk.WORD)
         self.textbox.pack()
-        self.after(100, self.ReadSerial())
+        self.textbox.after(100, self.ReadSerial)
 
     def ReadSerial(self):
-        if (ser.in_waiting > 0) :
-            input = ser.readline()
-            text = input.decode('utf-8').rstrip()
-            self.textbox.insert(tk.INSERT, text + ',')
-            self.textbox.pack()
-
-        self.after(100, self.ReadSerial())
+        input = ser.readline()
+        text = input.decode('utf-8').rstrip()
+        self.textbox.insert(tk.INSERT, text + ',')
+        self.textbox.pack()
+        self.textbox.after(100, self.ReadSerial)
 
 
 if __name__ == "__main__":
